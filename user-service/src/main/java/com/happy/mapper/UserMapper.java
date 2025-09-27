c695f2ddb1ebeccbbb355d97c92a8b0fbdd7a96b
hao1234good1234 <hao1234good1234@163.com>
2025-09-27 16:57:28 +0800

    修改usermapper文件

--- user-service/src/main/java/com/happy/mapper/UserMapper.java
+++ user-service/src/main/java/com/happy/mapper/UserMapper.java
@@ -37,7 +37,6 @@ public interface UserMapper {
             @Param("amount") BigDecimal amount
     );
 
-/*
     // freezeBalance: 冻结金额（可用余额减，冻结金额加）
     @Update("UPDATE users SET balance = balance - #{amount}, frozen_balance = frozen_balance + #{amount} " +
             "WHERE user_id = #{userId} AND balance >= #{amount}")
@@ -51,7 +50,6 @@ public interface UserMapper {
     @Update("UPDATE users SET balance = balance + #{amount}, frozen_balance = frozen_balance - #{amount} " +
             "WHERE user_id = #{userId}")
     int unfreezeBalance(@Param("userId") String userId, @Param("amount") BigDecimal amount);
-*/
 
 
 }
